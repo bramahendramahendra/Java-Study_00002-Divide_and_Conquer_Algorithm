70c08d6809980c9df703a0e319bf28ee619ae48b
Brama Hendra Mahendra <bramahendramahendra@gmail.com>
2019-08-22 15:04:50 +0700

    first commit

---
+++ src/pkg00001/divide/and/conquer/algorithm/DivideAndConquerAlgorithm.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg00001.divide.and.conquer.algorithm;
+
+/**
+ *
+ * @author Brama Hendra Mahendra
+ */
+public class DivideAndConquerAlgorithm {
+
+    public int binarySearch(int[] inputArr, int key) {
+         
+        int start = 0;
+        int end = inputArr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (key == inputArr[mid]) {
+                return mid+1;
+            }
+            if (key < inputArr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+  
+    public static void main(String[] args) {
+         
+        DivideAndConquerAlgorithm mbs = new DivideAndConquerAlgorithm();
+        int[] arr = {2, 4, 6, 8, 10, 12, 14, 16};
+        System.out.println("Key 14's position: "+mbs.binarySearch(arr, 14));
+        int[] arr1 = {6,34,78,123,432,900};
+        System.out.println("Key 432's position: "+mbs.binarySearch(arr1, 432));
+    }
+    
+}
